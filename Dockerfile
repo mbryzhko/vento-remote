3af2b732d4952fc68c4bd81de47255ac2f50a00d
Maksym Bryzhko <maxim1981@bigmir.net>
2021-04-14 21:05:20 +0300

    Fix timezone

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM arm32v6/openjdk:8u212-jre-alpine
 MAINTAINER Maksym Bryzhko <maxim.bryzhko@gmail.com>
 
+RUN apk update && \
+    apk add --no-cache tzdata
+
 # Add the service itself
 ARG JAR_FILE
 ADD target/${JAR_FILE} /usr/share/vento-remote/vento-remote.jar
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VENTO_CONFIG_PATH=${VENTO_CONFIG_PATH:-"./src/main/resources/schedule.yaml"}
-VENTO_VERSION=${VENTO_VERSION:-"1.7"}
+VENTO_VERSION=${VENTO_VERSION:-"1.11"}
 
 docker run -d --name vento-remote -v ${VENTO_CONFIG_PATH}:/usr/share/vento-remote/schedule.yaml \
   -e VENTO_SCHEDULE="file:/usr/share/vento-remote/schedule.yaml" \
