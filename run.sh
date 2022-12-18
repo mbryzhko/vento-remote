22dd57613acfdfdaf00d18e3cc961f17e0227095
Maksym Bryzhko <maxim.bryzhko@gmail.com>
2022-12-18 22:34:07 +0200

    Fixed run script

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 VENTO_CONFIG_PATH=${VENTO_CONFIG_PATH:-"./src/main/resources/schedule.yaml"}
 VENTO_VERSION=${VENTO_VERSION:-"1.11"}
 
-docker run -d --name vento-remote -v ${VENTO_CONFIG_PATH}:/usr/share/vento-remote/schedule.yaml \
+sudo docker run -d --name vento-remote -v ${VENTO_CONFIG_PATH}:/usr/share/vento-remote/schedule.yaml \
   -e VENTO_SCHEDULE="file:/usr/share/vento-remote/schedule.yaml" \
-  -e TZ="$(date +%Z)" \
-  docker.pkg.github.com/mbryzhko/vento-remote/vento-remote-arm32v6:$VENTO_VERSION
+  -e TZ="$(cat /etc/timezone)" \
+  --restart=always \
+  ghcr.io/mbryzhko/vento-remote/vento-remote-arm32v6:$VENTO_VERSION
