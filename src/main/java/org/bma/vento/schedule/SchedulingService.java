b4dd2a65f295e2882387213169949b957e8936f8
Maksym Bryzhko <maxim.bryzhko@gmail.com>
2023-02-08 19:01:06 +0200

    Handling missed executions

--- src/main/java/org/bma/vento/schedule/SchedulingService.java
+++ src/main/java/org/bma/vento/schedule/SchedulingService.java
@@ -2,13 +2,16 @@ package org.bma.vento.schedule;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.bma.vento.schedule.durable.DurableScheduleScenario;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.CronTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.scheduling.support.SimpleTriggerContext;
 
-import java.util.TimeZone;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,13 +19,62 @@ public class SchedulingService implements SchedulingConfigurer {
 
     private final ScheduleScenarioFactory scheduleScenarioFactory;
 
+    private final ScheduleProperties properties;
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        scheduleScenarioFactory.getScenarioToSchedule().forEach(scenario -> {
+        Collection<ScheduleScenario> scenarios = scheduleScenarioFactory.getScenarioToSchedule();
+
+        handleMissedExecution(scenarios);
+
+        scenarios.forEach(scenario -> {
             CronTrigger trigger = new CronTrigger(scenario.getCronExp(), TimeZone.getDefault());
 
             log.info("Scheduling scenario: {}, Next run at: {}", scenario, trigger.nextExecutionTime(new SimpleTriggerContext()));
             taskRegistrar.addCronTask(new CronTask(scenario, trigger));
         });
+    }
+
+    /**
+     * If durability is enabled then check if there are missed executions since last time.
+     */
+    private void handleMissedExecution(Collection<ScheduleScenario> scenarios) {
+        if (!properties.isDurabilityEnabled()) return;
+
+        // Keep scenario with missed executions ordered by last execution time
+        TreeMap<Date, DurableScheduleScenario> scenarioMissedExecution = new TreeMap<>();
+
+        collectScenarioWithMissedExecutions(scenarios, scenarioMissedExecution);
+
+        while (!scenarioMissedExecution.isEmpty()) {
+            Map.Entry<Date, DurableScheduleScenario> entry = scenarioMissedExecution.pollFirstEntry();
+            Date lastExecTime = entry.getKey();
+            DurableScheduleScenario scenario = entry.getValue();
+
+            Date nextExecutionTime = deriveNextExecutionTime(scenario, lastExecTime);
+
+            if (nextExecutionTime.before(new Date())) {
+                scenario.run();
+                scenarioMissedExecution.put(nextExecutionTime, scenario);
+            }
+        }
+    }
+
+    private static void collectScenarioWithMissedExecutions(Collection<ScheduleScenario> allScenarios,
+                                                            TreeMap<Date, DurableScheduleScenario> missedExecutions) {
+        allScenarios.stream()
+                .filter(DurableScheduleScenario.class::isInstance)
+                .map(DurableScheduleScenario.class::cast)
+                .filter(s -> s.getLastExecutionTime().isPresent())
+                .forEach(s -> missedExecutions.put(fromLocalDateTime(s.getLastExecutionTime().get()), s));
+    }
+
+    private static Date deriveNextExecutionTime(DurableScheduleScenario s, Date lastExecTime) {
+        CronTrigger trigger = new CronTrigger(s.getCronExp(), TimeZone.getDefault());
+        return trigger.nextExecutionTime(new SimpleTriggerContext(lastExecTime, lastExecTime, lastExecTime));
+    }
+
+    private static Date fromLocalDateTime(LocalDateTime ldt) {
+        return Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
--- src/test/java/org/bma/vento/schedule/SchedulingServiceTest.java
+++ src/test/java/org/bma/vento/schedule/SchedulingServiceTest.java
@@ -1,25 +1,32 @@
 package org.bma.vento.schedule;
 
-import org.bma.vento.client.VentoClient;
-import org.bma.vento.cmd.TurnOnCommand;
+import org.bma.vento.cmd.Command;
+import org.bma.vento.schedule.durable.DurableScheduleScenario;
+import org.bma.vento.schedule.durable.ScenarioState;
+import org.bma.vento.schedule.durable.ScenarioStateStore;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.scheduling.config.CronTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Collections;
 
 @ExtendWith(MockitoExtension.class)
 class SchedulingServiceTest {
 
     private static final String SCENARIO_NAME = "Test Scenario";
-    private static final String SCENARIO_CRON_EXP = "0 0/2 * ? * *";
+    private static final String SCENARIO_CRON_EXP_TPL = "%d %d * ? * *";
+
+    ScheduleProperties scheduleProperties = new ScheduleProperties();
+
     @Mock
     ScheduleScenarioFactory scheduleScenarioFactory;
 
@@ -27,27 +34,97 @@ class SchedulingServiceTest {
     ScheduledTaskRegistrar taskRegistrar;
 
     @Mock
-    VentoClient client;
+    Command testCommand;
 
-    @InjectMocks
+    @Mock
+    Command testCommand1;
+
+    @Mock
+    ScenarioStateStore store;
+
     SchedulingService service;
+
+    @BeforeEach
+    public void setup() {
+        service = new SchedulingService(scheduleScenarioFactory, scheduleProperties);
+    }
 
     @Test
     public void verifyCronTasksAreRegistered() {
-        Mockito.when(scheduleScenarioFactory.getScenarioToSchedule())
-                .thenReturn(Collections.singletonList(testScenario()));
+        givenScheduledScenario(simpleScenario(), durableScenario());
 
         service.configureTasks(taskRegistrar);
 
         ArgumentCaptor<CronTask> taskCaptor = ArgumentCaptor.forClass(CronTask.class);
-        Mockito.verify(taskRegistrar).addCronTask(taskCaptor.capture());
+        Mockito.verify(taskRegistrar, Mockito.times(2)).addCronTask(taskCaptor.capture());
 
-        Assertions.assertEquals(SCENARIO_CRON_EXP, taskCaptor.getValue().getExpression());
+        Assertions.assertNotNull(taskCaptor.getValue().getExpression());
         Assertions.assertTrue(taskCaptor.getValue().getRunnable() instanceof ScheduleScenario);
     }
 
-    private ScheduleScenario testScenario() {
-        TurnOnCommand command = new TurnOnCommand(client, new CommandProperties());
-        return new ScheduleScenario(SCENARIO_NAME, SCENARIO_CRON_EXP, Collections.singletonList(command));
+    @Test
+    public void shouldTriggerMissedScenarioRunWhenDurable() {
+        scheduleProperties.getDurabilityProperties().setEnable(true);
+
+        ScheduleScenario s = durableScenario();
+        ScheduleScenario s2 = durableScenario1();
+        givenScheduledScenario(s, s2);
+
+        Mockito.when(store.readState(Mockito.eq(s.getName())))
+                .thenReturn(new ScenarioState(LocalDateTime.now().minusHours(2)));
+
+        Mockito.when(store.readState(Mockito.eq(s2.getName())))
+                .thenReturn(new ScenarioState(LocalDateTime.now().minusHours(1)));
+
+        service.configureTasks(taskRegistrar);
+
+        Mockito.verify(testCommand, Mockito.times(2)).run();
+        Mockito.verify(testCommand1).run();
+    }
+
+    @Test
+    public void shouldNotHandleMissingRunsWhenNoLastExecutionTime() {
+        scheduleProperties.getDurabilityProperties().setEnable(true);
+
+        givenScheduledScenario(durableScenario());
+
+        Mockito.when(store.readState(Mockito.eq(SCENARIO_NAME))).thenReturn(null);
+
+        service.configureTasks(taskRegistrar);
+
+        Mockito.verify(testCommand, Mockito.times(0)).run();
+    }
+
+    @Test
+    public void shouldNotHandleMissingRunsWhenDurabilityIsNotEnabled() {
+        scheduleProperties.getDurabilityProperties().setEnable(false);
+
+        givenScheduledScenario(simpleScenario());
+
+        service.configureTasks(taskRegistrar);
+
+        Mockito.verify(testCommand, Mockito.times(0)).run();
+        Mockito.verifyZeroInteractions(store);
+    }
+
+    private ScheduleScenario simpleScenario() {
+        return new ScheduleScenario(SCENARIO_NAME, createSchedulingExpression(), Collections.singletonList(testCommand));
+    }
+
+    private ScheduleScenario durableScenario() {
+        return new DurableScheduleScenario(SCENARIO_NAME, createSchedulingExpression(), Collections.singletonList(testCommand), store);
+    }
+
+    private ScheduleScenario durableScenario1() {
+        return new DurableScheduleScenario("Scenario 1", createSchedulingExpression(), Collections.singletonList(testCommand1), store);
+    }
+
+    private void givenScheduledScenario(ScheduleScenario... scenario) {
+        Mockito.when(scheduleScenarioFactory.getScenarioToSchedule()).thenReturn(Arrays.asList(scenario));
+    }
+
+    private String createSchedulingExpression() {
+        LocalDateTime nextSchedulingDate = LocalDateTime.now().plusHours(1);
+        return String.format(SCENARIO_CRON_EXP_TPL, nextSchedulingDate.getSecond(), nextSchedulingDate.getMinute());
     }
 }
