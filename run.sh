79acdaac84999db0e12f0ba220efc48c472320c0
Maksym Bryzhko <maxim.bryzhko@gmail.com>
2026-05-15 22:51:29 +0300

    Update run sh

--- run.sh
+++ run.sh
@@ -8,9 +8,15 @@ if [ -f "/etc/timezone" ]; then
     VENTO_TZ="$(cat /etc/timezone)"
 fi
 
+if sudo docker ps -a --format '{{.Names}}' | grep -q '^vento-remote$'; then
+    echo "Stopping and removing existing vento-remote container..."
+    sudo docker stop vento-remote
+    sudo docker rm vento-remote
+fi
+
 sudo docker run -d --name vento-remote -v $VENTO_CONFIG_PATH:/usr/share/vento-remote/schedule.yaml \
   -e VENTO_SCHEDULE="file:/usr/share/vento-remote/schedule.yaml" \
   -e TZ="$VENTO_TZ" \
   --restart=always \
-  --security-opt seccomp=custom-seccomp.json
+  --security-opt seccomp=custom-seccomp.json \
   "$VENTO_IMAGE":"$VENTO_VERSION"
